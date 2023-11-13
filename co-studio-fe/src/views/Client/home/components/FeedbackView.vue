dd15162a5a6a3948a9c42ce57e1ba581af895658
Hoang21002 <dinhhoang026@gmail.com>
2023-11-13 14:18:51 +0700

    feat: feedback home 10%

--- co-studio-fe/src/views/Client/home/components/FeedbackView.vue
+++ co-studio-fe/src/views/Client/home/components/FeedbackView.vue
@@ -1,60 +1,57 @@
 <template>
-  <!-- <div class="parent flex items-center justify-center">
-        <div class=" relative h-screen">
-
-            <div class="div1 relative top-1/2 w-1/3 h-screen">
-                <img class="" src="../images/imgbannercenter.jpg" alt="">
-                <div class="div2 absolute top-2/4 translate-x-full -translate-y-full">
-                    <img class="" src="../images/imgbannercenter.jpg" alt="">
-                </div>
-                <div class="absolute top-2/4 dsd">
-                    <div class="div3 relative">
-                        <img class="" src="../images/imgbannercenter.jpg" alt="">
-                        <div class="div4 absolute top-2/4 translate-x-full -translate-y-full">
-                            <img class="" src="../images/imgbannercenter.jpg" alt="">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-    </div> -->
+  <div class="">
+    <div class="text-center mb-9">
+      <h1 class="font-spectral text-4xl font-bold text-DBrowColor">PHẢN HỒI TỪ KHÁCH HÀNG</h1>
+      <h4 class="font-lato text-xl font-light text-BrowColor">100% thực hiện bởi Cỏ Studio</h4>
+    </div>
+  </div>
+  <div class="parent">
+    <div class="div1 relative">
+      <div class="bg">
+        <div class="bg"></div>
+        <div class="bg"></div>
+        <div class="bg"></div>
+      </div>
+    </div>
+    <!-- <div class="div2"><div class="bg"></div></div>
+    <div class="div3"><div class="bg"></div></div>
+    <div class="div4"><div class="bg"></div></div>
+    <div class="div5">c</div>
+    <div class="div6">d</div> -->
+  </div>
 </template>
 
 <script></script>
 
 <style scoped>
-  .dsd {
-    transform: translate(200%, -150%);
-    /* --tw-translate-y: -158%;
-    --tw-translate-x: 200%; */
-  }
-
   /* .parent {
     display: grid;
     grid-template-columns: repeat(5, 1fr);
     grid-template-rows: repeat(5, 1fr);
     grid-column-gap: 0px;
     grid-row-gap: 0px;
-}
-
-.div1 {
+  } */
+  .bg {
+    background: url(../images/imgbannerleft.jpg) no-repeat center/cover;
+    width: 217px;
+    height: 333px;
+  }
+  /* .div1 {
     grid-area: 5 / 1 / 6 / 2;
-}
-
-.div2 {
+  }
+  .div2 {
     grid-area: 4 / 2 / 5 / 3;
-}
-
-.div3 {
+  }
+  .div3 {
     grid-area: 3 / 3 / 4 / 4;
-}
-
-.div4 {
+  }
+  .div4 {
     grid-area: 2 / 4 / 3 / 5;
-}
-
-.div5 {
-    grid-area: 1 / 5 / 2 / 6;
-} */
+  }
+  .div5 {
+    grid-area: 2 / 1 / 4 / 3;
+  }
+  .div6 {
+    grid-area: 5 / 4 / 6 / 6;
+  } */
 </style>
